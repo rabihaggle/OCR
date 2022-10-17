128989b3e37a30642188ca6ac97014b31d42218b
rabihaggle <90660792+rabihaggle@users.noreply.github.com>
2022-10-17 10:04:14 +0200

    Resueltas las Advertencias

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Procesar";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // richTextBox1
             // 
@@ -57,7 +57,7 @@
             this.richTextBox1.Size = new System.Drawing.Size(872, 398);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
             // 
             // button2
             // 
@@ -68,7 +68,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Salir";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // textBox1
             // 
--- Form1.cs
+++ Form1.cs
@@ -18,14 +18,12 @@ namespace OCR
             if (this == null) return;
             progressBar1.Value = (int)value;
         }
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
             this.Invoke(new RefreshProgressDelegate(RefreshProgress), 30);
-            OpenFileDialog ofd = new OpenFileDialog();
+            OpenFileDialog ofd = new();
             DialogResult dd = ofd.ShowDialog();
-
-
-            
+                        
             if (dd == DialogResult.OK)
             {
                 progressBar1.PerformStep();
@@ -34,21 +32,19 @@ namespace OCR
 
                 textBox1.Text = ofd.FileName;
 
-                using (var input = new OcrInput())
-                {
-                    input.AddPdf(ofd.FileName, textBox2.Text);
-                    var Result = Ocr.Read(input);
-                    this.Invoke(new RefreshProgressDelegate(RefreshProgress), 75);
-                    Console.WriteLine(Result.Text);
-                    MessageBox.Show($"{Result.Pages.Count()} páginas procesadas","Terminado");
-                    Console.WriteLine($"{Result.Pages.Count()} páginas procesadas");
+                using var input = new OcrInput();
+                input.AddPdf(ofd.FileName, textBox2.Text);
+                var Result = Ocr.Read(input);
+                this.Invoke(new RefreshProgressDelegate(RefreshProgress), 75);
+                Console.WriteLine(Result.Text);
+                MessageBox.Show($"{Result.Pages.Length} páginas procesadas", "Terminado");
+                Console.WriteLine($"{Result.Pages.Length} páginas procesadas");
 
-                    richTextBox1.Text = Result.Text;
+                richTextBox1.Text = Result.Text;
 
 
-                    this.Invoke(new RefreshProgressDelegate(RefreshProgress), 100);
-                    // 1 page for every page of the PDF
-                }
+                this.Invoke(new RefreshProgressDelegate(RefreshProgress), 100);
+                // 1 page for every page of the PDF
 
             }
             else
@@ -63,28 +59,20 @@ namespace OCR
 
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Button2_Click(object sender, EventArgs e)
         {
-            DialogResult dialog = new DialogResult();
-
-            dialog = MessageBox.Show("Desea salir de la aplicación ?", "Alert!", MessageBoxButtons.YesNo);
-
+            DialogResult dialog = MessageBox.Show("Desea salir de la aplicación ?", "Alert!", MessageBoxButtons.YesNo);
             if (dialog == DialogResult.Yes)
             {
                 System.Environment.Exit(1);
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-
-
-        }
+        
     }
 }
